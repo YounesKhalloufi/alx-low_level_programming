771c21eff24728da2fe1bfa6c14699677e817767
younes khalloufi <younes.khaloufi.1998@gmail.com>
2023-03-22 03:48:57 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-isalpha.c -o 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
 /**
  * _isalpha - checks for alphabet character
+ * letter Another cases, shows 0
+ *
  * @c: character to check
+ *
  * Return: 1 if lower or uppercase, 0 if not alphabet character
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
