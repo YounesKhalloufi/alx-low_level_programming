c659059b7acf8956cd0cfa959fdacfe9fc72d1e4
younes khalloufi <younes.khaloufi.1998@gmail.com>
2023-03-22 03:24:22 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print lowercase alphabet ten times
- * Return: void
+ * print_alphabet_x10 - print lowercase alphabet ten times.
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1 ; i <= 10; i++)
+	for (j = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
