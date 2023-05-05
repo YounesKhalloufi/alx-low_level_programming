3ded61036dbb8f6959ab0a1b760db00896c57f1d
younes khalloufi <younes.khaloufi.1998@gmail.com>
2023-05-04 17:00:15 -0700

    younes

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int ii, count = 0;
+	unsigned long int current;
+
+	for (ii = 63; ii >= 0; ii--)
+	{
+		current = n >> ii;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
